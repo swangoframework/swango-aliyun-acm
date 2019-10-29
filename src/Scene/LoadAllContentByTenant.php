4fd10e14b2a42f24102f8a13114463b4cbf7e50e
Tree <tree@brea.co>
2019-10-29 14:36:29 +0800

    bug fix

--- src/Scene/LoadAllContentByTenant.php
+++ src/Scene/LoadAllContentByTenant.php
@@ -1,14 +1,12 @@
 <?php
 namespace Swango\Aliyun\Acm\Scene;
 use Swango\Aliyun\Acm\Action\GetAllConfigByTenant;
-use Swango\Aliyun\Acm\Action\GetConfig;
 use Swango\Aliyun\Acm\LocalContents;
 class LoadAllContentByTenant {
     public static function load() {
         $action = new GetAllConfigByTenant();
         foreach ($action->getResult()['pageItems'] as $item) {
-            $content = new GetConfig($item['group'], $item['dataId']);
-            LocalContents::put($item['group'], $item['dataId'], $content);
+            LocalContents::get($item['group'], $item['dataId'], true);
         }
     }
 }
